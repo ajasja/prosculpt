790c2eb4856845b31b33e19e23c4990a926a92d9
FAOlivieri <federicoaolivieri@gmail.com>
2024-07-18 15:52:12 +0200

    Now slurm_runner can deal with output_dir being in the yaml and there being multiple tasks

--- slurm_runner.py
+++ slurm_runner.py
@@ -3,8 +3,9 @@ import shlex
 import time
 import os
 import argparse
+import yaml
 
-parser=argparse.ArgumentParser(epilog=('#### Any other arguments passed will be passed as they are to prosculpt ####\n'))
+parser=argparse.ArgumentParser(epilog=('#### Any other arguments passed will be passed as they are to prosculpt. If including output_dir, please include it first ####\n'))
 
 parser.add_argument('num_tasks', help='Number of concurrent jobs to run.')
 parser.add_argument('name', help='Desired task name')
@@ -33,11 +34,28 @@ out_command_file = f"ps2slurm_{task_name}_{int(time.time())}.txt"
 with open(out_command_file, 'w') as f:
     for i in range(n):
         let_argsv = args[1].copy()
+
+        output_argn=-1
+        for argn, arg in enumerate(let_argsv):
+            if "output_dir" in arg: output_argn=argn
+            if "-cd" in arg: config_file_folder_argn=argn+1
+            if "-cn" in arg: config_file_name_argn=argn+1
+
         if len(let_argsv)>0:
-            if "output_dir" in let_argsv[0]:
-                if let_argsv[0][-1:]!="/":
-                    let_argsv[0]+="/"
-                let_argsv[0] += f"{i:02d}"
+            if output_argn != -1: #If output_dir is in the let_argsv
+                if let_argsv[output_argn][-1:]!="/":
+                    let_argsv[output_argn]+="/"
+                let_argsv[output_argn] += f"{i:02d}"
+            else:  #if not, it is in the yaml file. Read it and modify it if there are more than 1 task
+                with open(os.path.join(slurm_runner_path,let_argsv[config_file_folder_argn],(let_argsv[config_file_name_argn])+".yaml"), "r") as yaml_file:
+                    data = yaml.safe_load(yaml_file)
+                output_dir=data["output_dir"]
+                if output_dir[-1:]!="/":
+                    output_dir+="/"
+                output_dir += f"{i:02d}"
+                let_argsv.append("++output_dir="+output_dir) #This will override the output present in the yaml file
+                #print("+output_dir="+output_dir)
+
         cmdline = " ".join(map(shlex.quote, let_argsv)) #join all arguments passed that aren't number of tasks or task name
         line = f"""python {slurm_runner_path}/rfdiff_mpnn_af2_merged.py {cmdline}"""
         print(line, file=f)
