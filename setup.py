b0f8bf8df4d3aa9b1b6f034866c31cf1aab6fd4c
Ajasja Ljubetič <ajasja.ljubetic@gmail.com>
2025-05-20 17:22:47 +0200

    Update setup.py
    Fixed path to script.

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='prosculpt',
     version='0.1.0',
     url='https://github.com/ajasja/prosculpt',
-    author='Nej Bizjak',
-    author_email='nejbizjak@gmail.com',
+    author='Ajasja Ljubetič',
+    author_email='ajasja.ljubetic@gmail.com',
     description='Package includes functions to protein strucutre analysis and data manipulation necessary',
     packages=find_packages(),
-    scripts=['rfdiff_mpnn_af2_disontiuous.py'],
+    scripts=['rfdiff_mpnn_af2_merged.py'],
     install_requires=[],
 )
