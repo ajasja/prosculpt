49a87d79f1063712a27b433f60a6d4e41fe658fa
FAOlivieri <federicoaolivieri@gmail.com>
2025-07-09 15:57:13 +0200

    modified setup.py to install dependencies

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='prosculpt',
-    version='0.1.0',
+    version='1.0.0',
     url='https://github.com/ajasja/prosculpt',
     author='Nej Bizjak',
     author_email='nejbizjak@gmail.com',
     description='Package includes functions to protein strucutre analysis and data manipulation necessary',
     packages=find_packages(),
-    scripts=['rfdiff_mpnn_af2_disontiuous.py'],
-    install_requires=[],
+    scripts=['rfdiff_mpnn_af2_merged.py', 'prosculpt.py', 'scripts/scoring_rg_charge_sap.py'],
+    install_requires=['pandas','omegaconf','hydra-core', 'pathlib', 'biopython','numpy'],
 )
